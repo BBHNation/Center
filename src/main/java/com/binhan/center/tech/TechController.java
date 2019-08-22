f67879101772464ef3bd87a2c43a680e9663f246
Hancock <binhan.bai@thoughtworks.com>
2019-08-22 17:03:15 +0800

    [TECH] - add tech controller to test CI/CD

--- src/main/java/com/binhan/center/tech/TechController.java
+++ src/main/java/com/binhan/center/tech/TechController.java
@@ -1,5 +1,16 @@
 package com.binhan.center.tech;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tech")
 public class TechController {
+    @GetMapping("/health")
+    public ResponseEntity<String> getHealthInfo() {
+        return ResponseEntity.ok("ok");
+    }
 
 }
