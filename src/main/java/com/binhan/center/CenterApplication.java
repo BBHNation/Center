90910d3b06a19ca57e1268b5d51b397fb9589487
Hancock <binhan.bai@thoughtworks.com>
2019-09-11 17:24:52 +0800

    [TECH] - add database docker.

--- src/main/java/com/binhan/center/CenterApplication.java
+++ src/main/java/com/binhan/center/CenterApplication.java
@@ -2,17 +2,9 @@ package com.binhan.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /** spring application */
-@SpringBootApplication(
-        exclude = {
-            DataSourceAutoConfiguration.class,
-            DataSourceTransactionManagerAutoConfiguration.class,
-            HibernateJpaAutoConfiguration.class
-        })
+@SpringBootApplication
 public class CenterApplication {
 
     /** @param args */
