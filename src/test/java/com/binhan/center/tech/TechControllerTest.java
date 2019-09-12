f210421aeff46141755a2842bea139059c7f828d
Hancock <binhan.bai@thoughtworks.com>
2019-09-12 17:25:15 +0800

    [TECH] - add one more test.

--- src/test/java/com/binhan/center/tech/TechControllerTest.java
+++ src/test/java/com/binhan/center/tech/TechControllerTest.java
@@ -1,9 +1,6 @@
 package com.binhan.center.tech;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -18,7 +15,7 @@ public class TechControllerTest extends WebApiTest {
     @Test
     public void shouldReturnSuccess_whenRequestHealth() throws IOException {
         // given
-        HttpUriRequest request = new HttpGet("http://localhost:" + this.port + "/tech/health");
+        HttpUriRequest request = new HttpGet(getBaseUrl() + "/tech/health");
 
         // when
         HttpResponse response = HttpClientBuilder.create().build().execute(request);
@@ -29,18 +26,18 @@ public class TechControllerTest extends WebApiTest {
                 convert(response.getEntity().getContent(), Charset.defaultCharset()), "ok");
     }
 
-    private String convert(InputStream inputStream, Charset charset) throws IOException {
+    @Test
+    public void shouldReturnSuccess_whenRequestWhoAmI() throws IOException {
+        // given
+        HttpUriRequest request = new HttpGet(getBaseUrl() + "/tech/whoami");
 
-        StringBuilder stringBuilder = new StringBuilder();
-        String line;
+        // when
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
 
-        try (BufferedReader bufferedReader =
-                new BufferedReader(new InputStreamReader(inputStream, charset))) {
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line);
-            }
-        }
-
-        return stringBuilder.toString();
+        // then
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
+        Assert.assertEquals(
+                convert(response.getEntity().getContent(), Charset.defaultCharset()),
+                "I am Hancock");
     }
 }
--- src/test/java/com/binhan/center/tech/WebApiTest.java
+++ src/test/java/com/binhan/center/tech/WebApiTest.java
@@ -1,5 +1,10 @@
 package com.binhan.center.tech;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +17,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource("classpath:application_test.properties")
 public class WebApiTest {
     @LocalServerPort public int port;
+
+    String getBaseUrl() {
+        return "http://localhost:" + port;
+    }
+
+    String convert(InputStream inputStream, Charset charset) throws IOException {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+
+        try (BufferedReader bufferedReader =
+                new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
 
     @Test
     public void temp() {}
