69a793ae02b492b94b3f7842d1272fa858d0eee3
Hancock <binhan.bai@thoughtworks.com>
2019-09-12 16:16:58 +0800

    [TECH] - add test properties. pass tests.

--- src/test/java/com/binhan/center/tech/WebApiTest.java
+++ src/test/java/com/binhan/center/tech/WebApiTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application_test.properties")
 public class WebApiTest {
     @LocalServerPort public int port;
 
