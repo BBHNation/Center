fc62d1e2fad2fd618d610123a66d06d080ee8712
BBHNAtion <bai.szsy@qq.com>
2019-09-17 16:22:28 +0800

    [WEEK_PLAN] - make applicationError private in package.

--- src/main/java/com/binhan/center/infrastructure/exception/ApplicationError.java
+++ src/main/java/com/binhan/center/infrastructure/exception/ApplicationError.java
@@ -3,11 +3,11 @@ package com.binhan.center.infrastructure.exception;
 import lombok.Getter;
 
 @Getter
-public class ApplicationError {
+class ApplicationError {
     private final int code;
     private final String message;
 
-    public ApplicationError(int code, String message) {
+    ApplicationError(int code, String message) {
         this.code = code;
         this.message = message;
     }
